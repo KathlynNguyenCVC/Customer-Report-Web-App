dd3b2dafdf1090fe458ae62036463c0e7d9c58a5
Kathlyn <knguyen@castleventures.com>
2020-02-27 18:12:04 -0500

    update

--- uploadform/views.py
+++ uploadform/views.py
@@ -1,5 +1,7 @@
 from django.shortcuts import render
 from django.http import HttpResponseRedirect
+from django.http import JsonResponse
+
 
 from .forms import UploadForm
 
@@ -9,10 +11,13 @@ def index(request):
         form = UploadForm(request.POST,request.FILES)
         if form.is_valid():
             form.save()
+            return JsonResponse({'error':False,'message':'success'})
+        else:
+            return JsonResponse('error': True, 'errors': form.errors)
     else:
         form = UploadForm()
-    context['form'] = form
-    return render(request,'index.html',context)
+        context['form'] = form
+        return render(request,'index.html',context)
 
 
 
